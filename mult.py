2bd41515d2f47860a6100957ce92dae06780f9cd
Lucas Claro <31224118+LucasClaroNicolau@users.noreply.github.com>
2018-11-05 11:18:18 -0200

    Começo da Implementação da Multiplicação
    Começo da Implementação da Multiplicação

---
+++ mult.py
@@ -0,0 +1,171 @@
+def maquina_mult(fita):
+    estado = -1
+    pos = 0
+    while (estado != 21):
+        marc = []
+        for i in range(pos):
+            marc.append(' ')
+        marc.append('|')
+        auxmarc = ''.join(marc)
+        print('{0} \n'.format(auxmarc))
+        aux = ''.join(fita)
+        print('{0} \n'.format(aux))
+        input()
+        print(estado,pos, fita[pos])
+        if (estado == -1):
+            pos += 1
+            estado += 1
+
+        elif (estado == 0):
+            if fita[pos] == '*':
+                pos += 1
+            else:
+                estado = 1
+                pos += 1
+                
+                
+        elif (estado == 1):
+            if (fita[pos] == '*'):
+                pos += 1
+            else:
+                estado = 2
+                pos -= 1
+                
+        elif (estado == 2):
+            if (fita[pos] == '*'):
+                estado = 3
+                fita[pos] = '$'
+                pos -= 1
+            elif(fita[pos] == '$'):
+                estado = 2
+                pos -= 1
+            else:
+                estado = 10
+                pos +=1
+                
+        elif (estado == 3):
+            if (fita[pos] == '*'):
+                pos -=1
+            elif (fita[pos] == '$'):
+                pos -= 1
+            else:
+                estado = 4
+                pos -=1
+                
+        elif (estado == 4):
+            if (fita[pos] == '*'):
+                pos -= 1
+            elif(fita[pos] == '_'):
+                estado = 10
+                pos += 1
+            elif(fita[pos] == '$'):
+                estado = 3
+                pos -=1
+            else:
+                estado =5
+                pos +=1
+                
+        elif (estado == 5):
+            if (fita[pos] == '*'):
+                estado = 6
+                fita[pos] = '$'
+                pos += 1
+            else:
+                estado = 9
+                pos -= 1
+                
+        elif (estado == 6):
+            if (fita[pos] == '*'):
+                pos+=1
+            else:
+                estado = 7
+                pos += 1
+                
+        elif (estado == 7):
+            if (fita[pos] == '_'):
+                estado = 8
+                pos += 1
+            else:
+                pos += 1
+                
+        elif (estado == 8):
+            if (fita[pos] == '*'):
+                pos += 1
+            else:
+                estado = 3
+                fita[pos] = '*'
+                pos -= 1
+                
+        elif (estado == 9):
+            if (fita[pos] == '>'):
+                estado = 0
+                pos += 1
+            else:
+                estado = 9
+                fita[pos] = '*'
+                pos -=1
+                
+        elif (estado == 10):
+            if (fita[pos] == '_'):
+                estado = 11
+                fita[pos] = '>'
+                pos -= 1
+            else:
+                pos += 1
+                
+        elif (estado == 11):
+            if (fita[pos] == '>'):
+                estado = 12
+                fita[pos] == '_'
+                pos += 1
+            else:
+                fita[pos] = '_'
+                pos -= 1
+                
+    marc = []
+    for i in range(pos):
+        marc.append(' ')
+    marc.append('|')
+    auxmarc = ''.join(marc)
+    print('{0} \n'.format(auxmarc))
+    aux = ''.join(fita)
+    print('{0} \n'.format(aux))
+    input()
+    p = 0;
+    for item in range(len(fita)):
+        if (fita[item] == '>'):
+            break
+        p += 1
+    for item in range(p):
+            fita.remove('_')
+    print('Fim \n')
+    print(''.join(fita))
+    input()
+    return
+
+
+# -----------------------------------------------------------------------------------------------------
+
+def monta_mult(a,b):
+    ret = ['>']
+    for item in range(a):
+        ret.append('*')
+
+    ret.append('_')
+
+    for item in range(b):
+        ret.append('*')
+    
+    for item in range(a*b):
+        ret.append('_')
+
+    return ret
+
+
+# -----------------------------------------------------------------------------------------------------
+def mult(v1, v2):
+   
+        fita = monta_mult(v1,v2)
+
+        maquina_mult(fita)
+mult(3,3)
